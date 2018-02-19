5d57bf73cd733d71366aa188e3e73030ddb1176a
iCrazyBlaze <icrazyblaze1@gmail.com>
2018-02-19 12:17:19 +0000

    :white_check_mark: fixed typo
    "occured"

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ pref = "//" #Command prefix
 des = "iCrazyBlaze's Discord.py Bot" #Description shown on using the help command
 
 # Error messages
-err_mesg = ":x: **An error occured!**"
+err_mesg = ":x: **An error occurred!**"
 err_mesg_pi = ":x: **This command can only be run when the bot is hosted on a Raspberry Pi.**"
 err_mesg_permission = ":x: **You don't have the permission to use this command.**"
 
