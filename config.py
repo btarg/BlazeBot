10ff25c0ec6a311e88a2688ed659b31d2f8ff093
iCrazyBlaze <iCrazyBlaze@users.noreply.github.com>
2018-03-20 17:58:09 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # BlazeBot configuration file (required to run)
 
 # Discord App token
-bbtoken = "" #REMOVE TOKEN BEFORE SYNC!! -Blaze
+bbtoken = "token here" #REMOVE TOKEN BEFORE SYNC!! -Blaze
 
 # YouTube API key (https://console.developers.google.com/apis/credentials/key)
-key = "AIzaSyDXnFGOjD4FzdFSv3zWvGRNDRAxV5AgJ4U" #REMOVE KEY BEFORE SYNC!! -Blaze
+key = "key here" #REMOVE KEY BEFORE SYNC!! -Blaze
 
 
 # This specifies what extensions to load when the bot starts up
