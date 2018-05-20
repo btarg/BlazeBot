097b8887a8db3508780048657a34b6ede475de36
iCrazyBlaze <icrazyblaze1@gmail.com>
2018-05-20 10:40:03 +0100

    :link: Command to generate invites

---
+++ cogs/invite.py
@@ -0,0 +1,23 @@
+# Make an invite link with 100 uses
+
+import discord
+from discord.ext import commands
+import config
+
+class invite():
+    def __init__(self, client):
+        self.client = client
+
+		
+		@commands.command(pass_context=True, aliases=[invitelink, serverlink, link])
+		async def invite(self, ctx):
+			invitelinknew = await self.client.create_invite(destination = ctx.message.channel, xkcd = True, max_uses = 100)
+			print(invitelinknew)
+			await self.client.say(invitelinknew)
+
+
+def setup(client):
+    client.add_cog(invite(client))
+
+
+
